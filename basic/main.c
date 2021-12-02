8bec8e374bd09ef5e91f22267a84ea9ded6ab2e3
Vladimir Marchenko <Vladimir.Marchenko@arm.com>
2021-12-02 09:50:34 +0100

    Added comments in main.c

--- basic/main.c
+++ basic/main.c
@@ -5,39 +5,48 @@
 #include "unity.h"
 #include <stdio.h>
 
-// In my_sum.c
+/* Application function to test */
 static int my_sum(int a, int b) {
   return a + b;
 }
 
+/*============= UNIT TESTS ============== */
+
+/* Called in RUN_TEST before executing test function */
 void setUp(void) {
   // set stuff up here
 }
 
+/* Called in RUN_TEST after executing test function */
 void tearDown(void) {
   // clean stuff up here
 }
 
+/* Testing summation of positive integers */
 static void test_my_sum_pos(void) {
   const int sum = my_sum(1, 1);
   TEST_ASSERT_EQUAL_INT(2, sum);
 }
 
+/* Testing summation of negative integers */
 static void test_my_sum_neg(void) {
   const int sum = my_sum(-1, -1);
   TEST_ASSERT_EQUAL_INT(-2, sum);
 }
 
+/* Testing summation of zeros */
 static void test_my_sum_zero(void) {
   const int sum = my_sum(0, 0);
   TEST_ASSERT_EQUAL_INT(0, sum);
 }
 
+/* Failing test with incorrect summation value */
 static void test_my_sum_fail(void) {
   const int sum = my_sum(1, -1);
   TEST_ASSERT_EQUAL_INT(2, sum);
 }
 
+/*  Main: Run tests */
 int main(void) {
   printf("---[ UNITY BEGIN ]---\n");
   UNITY_BEGIN();
