e6519a718b9adaf80020bd54a141430afe850dd8
Jonatan Antoni <jonatan.antoni@arm.com>
2022-03-09 14:51:01 +0100

    Python lint fixup

--- basic/build.py
+++ basic/build.py
@@ -11,8 +11,7 @@ from io import StringIO
 from junit_xml import TestSuite, TestCase, to_xml_report_string
 from os import environ
 from pathlib import Path
-from tempfile import NamedTemporaryFile
-from typing import AnyStr, Tuple
+from typing import Tuple
 
 from matrix_runner import main, matrix_axis, matrix_action, matrix_command, ConsoleReport, CropReport, ReportFilter
 
@@ -22,6 +21,7 @@ UNITTEST_BADGE_COLOR = {
     False: 'yellow',    # unstable: not all tests passing
     None: 'red'         # error: no test results generated
 }
+
 
 class UnityReport(ReportFilter):
     class Result(ReportFilter.Result, ReportFilter.Summary):
@@ -80,6 +80,7 @@ class UnityReport(ReportFilter):
         super(UnityReport, self).__init__()
         self.args = args
 
+
 @matrix_axis("target", "t", "The project target(s) to build")
 class TargetAxis(Enum):
     debug = ('debug')
@@ -90,6 +91,7 @@ def cbuild(config):
     """Build the config(s) with CMSIS-Build"""
     yield run_cbuild(config)
 
+
 @matrix_action
 def vht(config, results):
     """Run the config(s) with fast model"""
@@ -99,6 +101,7 @@ def vht(config, results):
     with open(f"vht-{ts}.log", "w") as file:
         results[0].output.seek(0)
         shutil.copyfileobj(results[0].output, file)
+
 
 @matrix_action
 def report(config, results):
@@ -116,21 +119,26 @@ def report(config, results):
     if 'GITHUB_WORKFLOW' in environ:
         print(f"::set-output name=badge::Unittest-{passed}%20of%20{executed}%20passed-{UNITTEST_BADGE_COLOR[passed == executed]}")
 
+
 @matrix_command(needs_shell=False)
 def run_cbuild(config):
     return ["bash", "-c", f"cbuild.sh --quiet basic.{config.target}.cprj"]
 
+
 @matrix_command(test_report=ConsoleReport()|CropReport("---\[ UNITY BEGIN \]---", '---\[ UNITY END \]---')|UnityReport())
 def run_vht(config):
     return ["VHT_Corstone_SSE-300_Ethos-U55", "-q", "--stat", "--simlimit", "1", "-f", "vht_config.txt", "Objects/basic.axf"]
+
 
 @matrix_command(needs_shell=True, test_report=ConsoleReport()|CropReport("---\[ UNITY BEGIN \]---", '---\[ UNITY END \]---')|UnityReport())
 def cat_log(log):
     cwd = Path.cwd().as_posix().replace('/','\\/')
     return ["bash", "-c", f"\"cat {log} | sed 's/\\/home\\/ubuntu\\/vhtwork/{cwd}/'\""]
 
+
 def timestamp(t: datetime = datetime.now()):
     return t.strftime("%Y%m%d%H%M%S")
+
 
 if __name__ == "__main__":
     main()
